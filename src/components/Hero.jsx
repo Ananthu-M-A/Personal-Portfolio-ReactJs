a825f004416ae0cbb87e0134a94e49a7fc26e97e
Ananthu M A <heartchaserananthu@gmail.com>
2024-08-01 11:51:50 +0530

    Alignment issues fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,24 +2,35 @@ import profile from '../assets/profile.jpg'
 import { HERO_CONTENT } from '../constants/constants'
 import { motion } from "framer-motion"
 
+const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.5, delay: delay }
+    }
+})
+
 const Hero = () => {
     return (
         <div className="border-neutral-800 border-b pb-4 lg:mb-35">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
-                    <div className="flex flex-col item-center lg:items-start">
-                        <motion.h1 initial={{ x: 0, opacity: 0 }} animate={{ x: 100, opacity: 1 }} transition={{ duration: 0.5, delay: 0.25 }}
-                            className="pb-5 text-5xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Ananthu M A</motion.h1>
-                        <motion.span initial={{ x: -50, opacity: 0 }} animate={{ x: 100, opacity: 1 }} transition={{ duration: 0.75, delay: 0.35 }} className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tighter text-transparent">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <motion.h1 variants={container(0)} initial="hidden" animate="visible"
+                            className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Ananthu M A</motion.h1>
+                        <motion.span variants={container(0.5)} initial="hidden" animate="visible"
+                            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tighter text-transparent">
                             Full Stack Developer
                         </motion.span>
-                        <motion.p initial={{ x: -50, opacity: 0 }} animate={{ x: 100, opacity: 1 }} transition={{ duration: 1, delay: 0.45 }} className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
+                        <motion.p variants={container(1)} initial="hidden" animate="visible"
+                            className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>
-                <div className="w-full lg:w-1/2 lg:p-8 mb-20">
+                <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <motion.img src={profile} width={400} alt="Ananthu M A"
-                            initial={{ x: 50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.55 }} />
+                        <motion.img src={profile} alt="Ananthu M A"
+                            initial={{ x: 100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 1.2 }} />
                     </div>
                 </div>
             </div>
